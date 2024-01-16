44a430b3a938f135cfb7b7259c75a9aa52fe5fa0
paulohenr96 <paulohrq_96@hotmail.com>
2024-01-16 17:50:24 -0300

    deleting

--- estudandoconfig/src/main/java/com/paulo/estudandoconfig/dto/ChartDTO.java
+++
@@ -1,7 +0,0 @@
-package com.paulo.estudandoconfig.dto;
-
-import java.util.List;
-
-public record ChartDTO (List<Integer> xAxis,List<Integer> yAxis) {
-	
-}
--- estudandoconfig/src/main/java/com/paulo/estudandoconfig/dto/InfoDTO.java
+++
@@ -1,5 +0,0 @@
-package com.paulo.estudandoconfig.dto;
-
-import java.math.BigDecimal;
-
-public record InfoDTO (BigDecimal income,Long totalProduct,Integer sumQuantity) {}
